ad34403ea69cdec4cec6d455e92c4ee872b54d7c
Celso Ernani <celsoernani@MacBook-Air-de-Celso.local>
2026-02-26 18:58:31 -0300

    fix env import

--- store-api/src/index.ts
+++ store-api/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import productsRouter from "./routes/products";
-import ordersRouter from "./routes/orders";
 
 dotenv.config();
+
+import productsRouter from "./routes/products";
+import ordersRouter from "./routes/orders";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productsRouter);
-app.use("/api/orders", productsRouter);
+app.use("/api/orders", ordersRouter);
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
